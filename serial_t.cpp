ab7d9652a2e0937d2da9a7101a4eb539335bd000
HeRunming <91403106+HeRunming@users.noreply.github.com>
2024-04-22 06:00:02 +0000

    by

--- serial_t.cpp
+++ serial_t.cpp
@@ -1,13 +1,9 @@
-/*
- * Foundations of Parallel Computing II, Spring 2024.
- * Instructor: Chao Yang, Xiuhong Li @ Peking University.
- * This is a serial implementation of LU decomposition.
- */
-
 #include <iostream>
 #include <fstream>
 #include <vector>
-#include <chrono> 
+#include <omp.h>
+#include <chrono>
+#include <iomanip>
 
 using namespace std;
 
@@ -57,6 +53,9 @@ namespace utils {
     }
 
     void lu_decomposition() {
+        // 开始计时
+        auto start = chrono::high_resolution_clock::now();
+        #pragma omp parallel for
         for (int j = 0; j < N; ++j) {
             for (int i = j + 1; i < N; ++i) {
                 L[i * N + j] = U[i * N + j] / U[j * N + j];
@@ -65,6 +64,13 @@ namespace utils {
                 }
             }
         }
+
+        // 结束计时
+        auto end = chrono::high_resolution_clock::now();
+        auto duration = chrono::duration_cast<chrono::microseconds>(end - start);
+
+        // 输出并行计算所用的时间
+        cout << "Parallel LU decomposition took " << duration.count() << " microseconds." << endl;
     }
 
     void write_outputs(const string& output_filename) {
@@ -92,18 +98,18 @@ namespace utils {
 
 int main(int argc, char* argv[]) {
     if (argc != 3)
-        utils::abort_with_error_message("ERROR: Invalid command. Usage: ./serial <input_file> <output_file>");
+        utils::abort_with_error_message("ERROR: Invalid command. Usage: ./parallel_lu <input_file> <output_file>");
 
     string input_filename = argv[1];
     string output_filename = argv[2];
 
     utils::read_inputs_and_initialize(input_filename);
 
-    auto start = std::chrono::high_resolution_clock::now();
+    // 设置OpenMP线程数
+    int num_threads = 8; // 您可以根据需要修改这个值
+    omp_set_num_threads(num_threads);
+
     utils::lu_decomposition();
-    auto end = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<chrono::microseconds>(end - start);
-    cout << "Parallel LU decomposition took " << duration.count() << " microseconds." << endl;
 
     utils::write_outputs(output_filename);
 
--- try.cpp
+++ try.cpp
@@ -13,16 +13,52 @@ namespace utils {
     vector<double> L; // lower triangular matrix
     vector<double> U; // upper triangular matrix
 
-    // ... 其他函数不变 ...
+    void abort_with_error_message(const string& msg) {
+        cerr << msg << endl;
+        abort();
+    }
+    /*
+     * The input file will be in the following format:
+     * - The first line is an integer N, the number of rows and columns in the matrix.
+     * - The following lines contain the matrix entries in row-major order.
+     *
+     * The output file will be in the following format:
+     * - The lower triangular matrix L and the upper triangular matrix U obtained from the LU decomposition, in row-major order.
+     *
+     * Run:
+     * $ ./serial <input_file> <output_file>
+     */
+    int read_inputs_and_initialize(const string& filename) {
+        ifstream inputf(filename);
+        if (!inputf.is_open())
+            abort_with_error_message("ERROR: Unable to open input file.");
+
+        inputf >> N;
+        mat.resize(N * N);
+        for (int i = 0; i < N * N; ++i) {
+            inputf >> mat[i];
+        }
+        inputf.close();
+
+        L.resize(N * N);
+        U.resize(N * N);
+
+        copy(mat.begin(), mat.end(), U.begin());
+
+        // Initialize L to identity matrix
+        for (int i = 0; i < N; ++i) {
+            L[i * N + i] = 1.0;
+        }
+        return 0;
+    }
 
     void lu_decomposition() {
         // 开始计时
         auto start = chrono::high_resolution_clock::now();
-
         for (int j = 0; j < N; ++j) {
+            #pragma omp parallel for
             for (int i = j + 1; i < N; ++i) {
                 L[i * N + j] = U[i * N + j] / U[j * N + j];
-#pragma omp parallel for collapse(2)
                 for (int k = j; k < N; ++k) {
                     U[i * N + k] -= L[i * N + j] * U[j * N + k];
                 }
@@ -38,7 +74,25 @@ namespace utils {
     }
 
     void write_outputs(const string& output_filename) {
-        // ... 省略 ...
+        ofstream outputf(output_filename);
+        if (!outputf.is_open())
+            abort_with_error_message("ERROR: Unable to open output file.");
+
+        outputf << "L matrix:" << endl;
+        for (int i = 0; i < N; ++i) {
+            for (int j = 0; j < N; ++j) {
+                outputf << L[i * N + j] << " ";
+            }
+            outputf << endl;
+        }
+        outputf << "U matrix:" << endl;
+        for (int i = 0; i < N; ++i) {
+            for (int j = 0; j < N; ++j) {
+                outputf << U[i * N + j] << " ";
+            }
+            outputf << endl;
+        }
+        outputf.close();
     }
 }
 
